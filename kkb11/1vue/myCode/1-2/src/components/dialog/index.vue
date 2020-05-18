33dc580f272d537f0f53c8dfcec21b6d0978167e
caoyanyuan <1689456277@qq.com>
2020-05-18 15:46:59 +0800

    01vue: 手写弹出框dialog

---
+++ kkb11/1vue/myCode/1-2/src/components/dialog/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <button @click="showDialog">点击</button>
+</template>
+
+<script>
+import CreateDialog from "./createDialog"
+import Dialog from "./dialog"
+
+export default {
+    methods: {
+        showDialog() {
+            let dialog = CreateDialog(Dialog, {
+                title: "手写弹出框",
+                message:"请求登录!" ,
+                duration: 100000
+            })
+            dialog.show()
+        }
+    },
+    components: {
+        Dialog
+    }
+}
+</script>
