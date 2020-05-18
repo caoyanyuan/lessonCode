f5749d199dd29cb0bb98dce3fbba5ad5623add04
caoyanyuan <1689456277@qq.com>
2020-05-18 15:36:03 +0800

    element form组件实现

---
+++ kkb11/1vue/myCode/1-2/src/components/form/formItem.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <label v-if="label">{{label}}</label>
+    <slot></slot>
+    <p v-if="errorMessage" style="color:red">{{errorMessage}}</p>
+  </div>
+</template>
+
+<script>
+//Schema 就是elementui用的验证框架
+import Schema from 'async-validator'
+export default {
+  inject: ["form"],
+  props: {
+    label: {
+      type: String,
+      default: ""
+    },
+    prop: {
+      type: String
+    }
+  },
+  data() {
+    return {
+      errorMessage: ""
+    };
+  },
+  mounted() {
+      this.$on('validate', this.validate)
+  },
+  methods: {
+      validate() {
+          // 做校验
+          const value = this.form.model[this.prop]
+          const rules = this.form.rules[this.prop]
+          // npm i async-validator -S
+          const desc = {[this.prop]: rules};
+          const schema = new Schema(desc);
+          // return的是校验结果的Promise
+          return schema.validate({[this.prop]: value}, errors => {
+              if (errors) {
+                  this.errorMessage = errors[0].message;
+              }else {
+                  this.errorMessage = ''
+              }
+              console.log(this.errorMessage)
+          })
+      }
+  },
+};
+</script>
