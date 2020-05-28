8f1b26b2c88706b6752f48d682ec9f714851eb13
caoyanyuan <1689456277@qq.com>
2020-05-28 17:27:38 +0800

    react: setState特性讨论

--- kkb11/2react/my-react/src/index.js
+++ kkb11/2react/my-react/src/index.js
@@ -1,1 +1,26 @@
-import React,{ Component } from "React"
+import React,{ Component } from "react"
+import ReactDom from "react-dom"
+
+import Calculator from "./components/temperature"
+
+class Clock extends Component {
+    constructor() {
+        super()
+
+        this.state = { counter: 0 }
+    }
+    componentDidMount() {
+     
+    }
+    changeValue = () => {
+      this.setState({counter: this.state.counter+1})
+      console.log(this.state.counter) // 1
+    }
+    render() {
+        return <div onClick={this.changeValue}>{this.state.counter}</div>
+    }
+}
+
+ReactDom.render(<Calculator/>, document.getElementById("root"))
+
+
